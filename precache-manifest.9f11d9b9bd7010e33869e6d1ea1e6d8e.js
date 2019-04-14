e7aeb00d3d7045fc8313ffd2d46a95de49b8e9cf
Af009 <afwebd@yahoo.com>
2019-04-14 18:28:00 +0300

    Updates

---
+++ precache-manifest.9f11d9b9bd7010e33869e6d1ea1e6d8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bc55fa9ca5c9adb3122",
+    "url": "/CSS-Tool/static/js/runtime~main.0bc55fa9.js"
+  },
+  {
+    "revision": "27e8a4bc7cef75bc8d1f",
+    "url": "/CSS-Tool/static/js/main.27e8a4bc.chunk.js"
+  },
+  {
+    "revision": "60afaf9710412e514a36",
+    "url": "/CSS-Tool/static/js/1.60afaf97.chunk.js"
+  },
+  {
+    "revision": "27e8a4bc7cef75bc8d1f",
+    "url": "/CSS-Tool/static/css/main.7dc36f07.chunk.css"
+  },
+  {
+    "revision": "37c9dbb296afee0bf3674b626baf42c5",
+    "url": "/CSS-Tool/index.html"
+  }
+];
